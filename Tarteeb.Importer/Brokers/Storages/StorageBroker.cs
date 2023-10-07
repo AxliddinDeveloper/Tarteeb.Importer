73b90711e2f0c29d2564c6248864d9bafd728757
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-10-07 10:33:23 +0500

    DATA: Model & Create Clients Table

---
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,41 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Tarteeb.Importer.Models.Clients;
+
+namespace Tarteeb.Importer.Brokers.Storages
+{
+    internal class StorageBroker : EFxceptionsContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public async Task<Client> InsertClientAsync(Client client)
+        {
+            await this.Clients.AddAsync(client);
+            await this.SaveChangesAsync();
+
+            return client;
+        }
+        public IQueryable<Client> SelectAllClients()
+        {
+            return this.Clients.AsQueryable();
+        }
+
+        public async Task<Client> SelectClientByIdAsync(Guid clientId) =>
+             await this.Clients.FindAsync(clientId);
+        
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source = ..\..\..\Tarteeb.db";
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
--- Tarteeb.Importer/Program.cs
+++ Tarteeb.Importer/Program.cs
@@ -3,7 +3,21 @@
 // Powering True Leadership
 //===========================
 
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using Microsoft.VisualBasic;
+using Tarteeb.Importer.Brokers.Storages;
+using Tarteeb.Importer.Models.Clients;
+
 namespace Tarteeb.Importer
 {
-
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");            
+        }
+    }
 }
